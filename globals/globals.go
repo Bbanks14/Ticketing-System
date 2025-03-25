432c09f711981bb3c605cf629277723c1da15438
Bbanks14 <boluwatifeoyeyemi23@gmail.com>
2025-03-25 02:05:48 +0100

    Random Monday Night

---
+++ globals/globals.go
@@ -0,0 +1,20 @@
+package globals
+
+import (
+	"github.com/Bbanks14/ticketing-system/structs"
+)
+
+// Tickets holds all the created tickets.
+var Tickets = make(map[string]structs.Ticket)
+
+// Mails hold all currently cached mails.
+var Mails = make(map[string]structs.Mail)
+
+// ServerConfig holds the given server config for access to backend systems.
+var ServerConfig *structs.ServerConfig
+
+// LogConfig contains the global logging configuration
+var LogCOnfig *structs.LogConfig
+
+// Sessions holds all the sessions for the users.
+var Sessions = make(map[string]structs.SessionManager)
